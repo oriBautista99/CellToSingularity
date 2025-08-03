472d6e7f858cbc4c7f50c49c42dfb00b4834b5ca
Oriana Bautista <oribautista99@gmail.com>
2025-08-03 11:34:31 -0400

    Game controller

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -1,4 +1,61 @@
 package controller;
 
+import model.*;
+import model.enums.EstadoElemento;
+
+import java.util.*;
+
 public class GameController {
+    private Jugador jugador;
+    private List<ElementoEvolutivo> todosLosElementos;
+    private Timer timer;
+
+    public GameController(Jugador jugador, List<ElementoEvolutivo> elementosIniciales) {
+        this.jugador = jugador;
+        this.todosLosElementos = elementosIniciales;
+        iniciarTimer();
+    }
+
+    private void iniciarTimer() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                generarRecursosPasivos();
+                verificarDesbloqueos();
+            }
+        }, 0, 1000); // cada 1 segundo
+    }
+
+    private void generarRecursosPasivos() {
+        for (ElementoEvolutivo e : jugador.getElementosActivos()) {
+            String tipo = e.getTipoRecursoProducido();
+            double cantidad = e.getProduccion();
+            jugador.aumentarRecurso(tipo, cantidad);
+        }
+    }
+
+    private void verificarDesbloqueos() {
+        for (ElementoEvolutivo e : todosLosElementos) {
+            if (e.getEstado() == EstadoElemento.BLOCKED && e.requisitosCumplidos(jugador.getElementosDesbloqueados())) {
+                e.desbloquear();
+            }
+        }
+    }
+
+    public void detener() {
+        if (timer != null) timer.cancel();
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public List<ElementoEvolutivo> getTodosLosElementos() {
+        return todosLosElementos;
+    }
+
+    public void hacerClick() {
+        jugador.hacerClick();
+    }
+
 }
