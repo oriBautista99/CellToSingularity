819ad9e1341094fbfc85c8496398bf9d0ad196c4
Oriana Bautista <oribautista99@gmail.com>
2025-08-03 10:27:33 -0400

    recurso class

--- src/model/Recurso.java
+++ src/model/Recurso.java
@@ -1,4 +1,46 @@
 package model;
 
 public class Recurso {
+    private String tipo;
+    private double cantidad;
+    private double tasaGeneracion;
+
+    public Recurso(String tipo) {
+        this.tipo = tipo;
+        this.cantidad = 0;
+        this.tasaGeneracion = 0;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public double getTasaGeneracion() {
+        return tasaGeneracion;
+    }
+
+    public void setTasaGeneracion(double tasaGeneracion) {
+        this.tasaGeneracion = tasaGeneracion;
+    }
+
+    public void aumentarCantidad(double cantidad) {
+        this.cantidad += cantidad;
+    }
+
+    public boolean consumir(double cantidad) {
+        if (this.cantidad >= cantidad) {
+            this.cantidad -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    public void reiniciar() {
+        this.cantidad = 0;
+        this.tasaGeneracion = 0;
+    }
 }
